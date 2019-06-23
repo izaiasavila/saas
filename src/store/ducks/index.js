10526638945da53c688c91c4e123948c14b4db5c
Izaias Avila da Conceicao Filho <izaias.filho@selfitacademias.com.br>
2019-06-23 13:47:22 -0300

    Adicionado alertas com toast

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 
+import { reducer as toastr } from 'react-redux-toastr';
+
 import { reducer as auth } from './auth';
 
 export default combineReducers({
   auth,
+  toastr,
 });
