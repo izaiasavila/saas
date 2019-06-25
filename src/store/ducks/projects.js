c694c402d475222e24545044642f709f449891a2
Izaias Avila da Conceicao Filho <izaias.filho@selfitacademias.com.br>
2019-06-24 21:03:11 -0300

    ajuste de nome de metodos de projetos

--- src/store/ducks/projects.js
+++ src/store/ducks/projects.js
@@ -24,18 +24,18 @@ const INITIAL_STATE = Immutable({
 
 /* Reducers */
 
-export const success = (state, { data }) => state.merge({ data });
+export const getSuccess = (state, { data }) => state.merge({ data });
 
 export const openModal = state => state.merge({ projectModalOpen: true });
 
 export const closeModal = state => state.merge({ projectModalOpen: false });
 
-export const createSuccess = (state, { project }) => state.merge({ data: [...state.data, project] });
+export const success = (state, { project }) => state.merge({ data: [...state.data, project] });
 
 /* Reducers to types */
 export const reducer = createReducer(INITIAL_STATE, {
-  [Types.GET_PROJECTS_SUCCESS]: success,
+  [Types.GET_PROJECTS_SUCCESS]: getSuccess,
   [Types.OPEN_PROJECT_MODAL]: openModal,
   [Types.CLOSE_PROJECT_MODAL]: closeModal,
-  [Types.CREATE_PROJECT_SUCCESS]: createSuccess,
+  [Types.CREATE_PROJECT_SUCCESS]: success,
 });
